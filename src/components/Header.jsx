68bfb975861ca428dbec503ee9f17cfa0db5a68f
Murilo Macedo <murilomacedosilva2@gmail.com>
2025-08-07 09:52:26 -0300

    first section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,8 +9,8 @@ function Header() {
             </div>
             <div className="flex items-center gap-3">
                 <div className=" flex flex-col items-end md:flex-row-reverse">
-                    <a href="" className="text-white font-Inter font-extralight md:pl-[25px] text-[15px]">@dra.v_luizmacedoadvogada</a>
-                    <a href="" className="text-white font-Inter font-extralight md:pr-[25px] md:border-r-white md:border-r text-[15px]">+55 11 99666-9191</a>
+                    <a href="" className="text-white font-Inter font-extralight md:pl-[25px] text-[12px] md:text-[15px]">@dra.v_luizmacedoadvogada</a>
+                    <a href="" className="text-white font-Inter font-extralight md:pr-[25px] md:border-r-white md:border-r text-[12px] md:text-[15px]">+55 11 99666-9191</a>
                 </div>
                 <div>
 
