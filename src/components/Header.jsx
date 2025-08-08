df30c5acfc1480e6718d54ff7f94ebc3a09a0ff4
Murilo Macedo <murilomacedosilva2@gmail.com>
2025-08-08 11:26:25 -0300

    section commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
 import menu from "../assets/menu.png"
 import logoHead from "../assets/logoHead.png"
 import { useEffect, useState } from "react";
+import emailLogo from "../assets/emailLogo.png"
+import instagranLogo from "../assets/instagranLogo.png"
+import linkedinLogo from "../assets/linkedinLogo.png"
+import wathsappLogo from "../assets/wathsappLogo.png"
 
 function Header() {
 
@@ -21,7 +25,7 @@ function Header() {
     })
 
     return (
-        <header id="Header" className={`fixed transition duration-500 z-5 top-0 w-screen px-[20px] xl:px-[30px] flex justify-between items-center ${scrolled ? 'bg-cyan-950/80 backdrop-blur-md h-[80px]' : 'bg-transparent h-[100px]'}`}>
+        <header id="Header" className={`fixed transition duration-500 z-5 top-0 w-screen px-[20px] xl:px-[30px] flex justify-between items-center ${scrolled ? 'bg-cyan-950/80 backdrop-blur-md h-[80px] shadow-lg' : 'bg-transparent h-[100px]'}`}>
             <div className="">
                 <img src={menu} alt="menu" className="w-[27px] cursor-pointer" />
             </div>
@@ -30,8 +34,19 @@ function Header() {
                     <a href="https://www.instagram.com/dra.v_luizmacedoadvogada/" target="_blank" className="text-white font-Inter font-extralight md:pl-[25px] text-[12px] md:text-[18px]">@dra.v_luizmacedoadvogada</a>
                     <a href="https://wa.me/+5511996669191" target="_blank" className="text-white font-Inter font-extralight md:pr-[25px] md:border-r-white md:border-r text-[12px] md:text-[18px]">+55 11 99666-9191</a>
                 </div>
-                <div>
-
+                <div className="lg:flex items-center justify-center gap-3.5 mx-[8px] hidden">
+                    <a href="">
+                        <img src={emailLogo} alt="botão para o email" className="w-[33px]" />
+                    </a>
+                    <a href="">
+                        <img src={wathsappLogo} alt="botão para o whatsapp" className="w-[33px]" />
+                    </a>
+                    <a href="">
+                        <img src={linkedinLogo} alt="botão para o linkedin" className="w-[33px]" />
+                    </a>
+                    <a href="">
+                        <img src={instagranLogo} alt="botão para o instagram" className="w-[33px]" />
+                    </a>
                 </div>
                 <a href="#Header">
                     <img src={logoHead} alt="logo" className={` transition duration-1000 ${scrolled ? 'w-[80px] h-[80px]' : 'w-[100px] h-[100px]'}`} />
--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -4,12 +4,12 @@ import backgroundInicio from "../assets/backgroundInicio.png"
 
 function Inicio() {
     return (
-        <section id="Inicio" className="bg-transparent md:h-[120vh] h-[160vh] mt-[100px] md:mt-0 left-0 w-screen flex flex-col pb-[30px] px-[20px] md:flex-row md:justify-center md:gap-15 md:items-center">
+        <section id="Inicio" className="bg-transparent md:h-[120vh] h-[160vh] mt-[100px] md:mt-0 left-0 w-screen flex flex-col pb-[30px] px-[20px] md:flex-row md:justify-center md:gap-10 md:items-center">
             <div className="text-amber-200">
-                <h1 className="text-[90px] font-Italiana bg-gradient-to-r from-orange-300 from-[0%] to-[80%] to-amber-200 text-shadow-lg bg-clip-text text-transparent md:text-[110px] md:leading-[100px] lg:text-[150px] leading-[90px] lg:leading-[130px] xl:leading-[150px] font-normal xl:text-[170px]">Viviane <br /> Luiz <br /> Macedo</h1>
+                <h1 className="text-[90px] font-Italiana bg-gradient-to-r from-orange-300 from-[0%] to-[80%] to-yellow-200 bg-clip-text text-transparent md:text-[110px] md:leading-[100px] lg:text-[150px] leading-[90px] lg:leading-[130px] xl:leading-[150px] font-normal xl:text-[170px]">Viviane <br /> Luiz <br /> Macedo</h1>
             </div>
             <div className=" max-w-[550px] gap-16 max-h-[500px]">
-                <h2 className="bg-gradient-to-r from-amber-200 to-orange-400 bg-clip-text text-transparent text-[20px] tracking-[15px] font-Inter font-semibold lg:text-[35px] text-shadow-lg">ADVOCACIA</h2>
+                <h2 className="bg-gradient-to-r from-amber-200 to-orange-400 bg-clip-text text-transparent text-[20px] tracking-[15px] font-Inter font-semibold lg:text-[35px]">ADVOCACIA</h2>
                 <p className="text-amber-200 text-[17px] lg:text-[22px] w-full mt-[20px] mb-[25px] font-Inika">Compliance estratégico e jurídico para mitigar riscos e impulsionar governança.</p>
                 <p className="text-white w-full text-[16px] font-Inika lg:text-[20px]">Atuação especializada em Direito Empresarial focada na integridade corporativa, prevenção de riscos e conformidade regulatória.</p>
                 <div className="md:flex md:justify-between mt-[50px] gap-7 w-full">
